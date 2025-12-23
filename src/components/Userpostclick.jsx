ca06f1dc1531d8f39c33110a626a92140259d355
apurva470 <apurvasolanki70@gmail.com>
2025-12-23 19:01:15 +0530

    changing userpostclick

--- src/components/Userpostclick.jsx
+++ src/components/Userpostclick.jsx
@@ -23,7 +23,7 @@ const UserPostclick = () => {
   }, [targetpostid])
   const { url, description, userPostViewCount, _id, createdAt, firstname, lastname ,viewedBy, author } = post;
   const photourl = post?.author?.photourl;
-  console.log(post?.author._id);
+  console.log(post?.author?._id);
   
   const style=firstname?.charAt(0).toUpperCase()+lastname?.charAt(0).toUpperCase();
   return (
@@ -49,7 +49,7 @@ const UserPostclick = () => {
                      border border-gray-200 overflow-hidden"
         >
           {/* User info */}
-          <Link to={"/user/" + author._id}>
+          <Link to={"/user/" + author?._id}>
             <div className="flex items-center p-4">
               <div className="w-12 h-12 rounded-full overflow-hidden mr-3 flex-shrink-0">
                 {photourl !== "http://peoplepost-default.png" ? (
