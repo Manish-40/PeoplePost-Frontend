8547278376533c270f97d0efb87645c51696881a
apurva470 <apurvasolanki70@gmail.com>
2026-01-13 15:07:52 +0530

    adding only http://localhost:3001 in constant and remove from .env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const baseurl=import.meta.env.VITE_BACKEND_URL
-export const baseurlIndicator=import.meta.env.VITE_BACKEND_ONLINE_INDICATOR_URL
+export const baseurlIndicator='http://localhost:3001'
 //import.meta.env.VITE_BACKEND_URL
 
